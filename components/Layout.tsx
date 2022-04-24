05a61c9e2102cea2659b79ea52e10f15fbdcff93
wozitto <hirookatoshito@gmail.com>
2022-04-24 17:25:22 +0900

    feat: header

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -1,6 +1,7 @@
 import React, { ReactNode } from 'react';
 import Link from 'next/link';
 import Head from 'next/head';
+import { BsGithub, BsTwitter } from 'react-icons/Bs';
 
 type Props = {
   children?: ReactNode;
@@ -14,21 +15,40 @@ const Layout = ({ children, title = 'This is the default title' }: Props) => (
       <meta charSet="utf-8" />
       <meta name="viewport" content="initial-scale=1.0, width=device-width" />
     </Head>
-    <header>
-      <nav>
-        <Link href="/">
-          <a>Home</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/about">
-          <a>About</a>
-        </Link>{' '}
-        |{' '}
-        <Link href="/users">
-          <a>Users List</a>
-        </Link>{' '}
-        | <a href="/api/users">Users API</a>
-      </nav>
+    <header className="bg-primary">
+      <div className="navbar">
+        <nav className="navbar-center bg-primary m-auto">
+          <Link href="/">
+            <a className="px-4 text-xl text-white no-underline hover:underline">
+              Home
+            </a>
+          </Link>
+          |
+          <Link href="/about">
+            <a className="px-4 text-xl text-white no-underline hover:underline">
+              About
+            </a>
+          </Link>
+          |
+          <Link href="/works">
+            <a className="px-4 text-xl text-white no-underline hover:underline">
+              Works
+            </a>
+          </Link>
+          |
+          <Link href="https://github.com/wozitto">
+            <a className="px-4 text-xl text-white no-underline hover:underline">
+              <BsGithub />
+            </a>
+          </Link>
+          |
+          <Link href="https://twitter.com/_wozitto_">
+            <a className="px-4 text-xl text-white no-underline hover:underline">
+              <BsTwitter />
+            </a>
+          </Link>
+        </nav>
+      </div>
     </header>
     {children}
     <footer>

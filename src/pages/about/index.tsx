e10c3f594672692ca0b15f622a7df79ebffd2ec3
wozitto <hirookatoshito@gmail.com>
2022-05-22 22:44:16 +0900

    time series switch

--- src/pages/about/index.tsx
+++ src/pages/about/index.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import { RiMoneyCnyCircleFill } from 'react-icons/ri';
 import { BsTsunami } from 'react-icons/bs';
 import { FaSchool, FaGraduationCap } from 'react-icons/fa';
@@ -11,6 +12,27 @@ import { ExternalLink } from '../../components/Link';
 import { Tag, Tags } from '../../components/Tag';
 
 const AboutPage = () => {
+  const [isTimeSeries, setIsTimeSeries] = useState(false);
+  const toggleClass = ' transform translate-x-5';
+
+  const TimeSeriesSwitch = () => (
+    <div
+      className={`mt-9 md:w-14 md:h-7 w-12 h-6 flex items-center rounded-full p-1 cursor-pointer ${
+        isTimeSeries ? 'bg-primary' : 'bg-gray-300'
+      }`}
+      onClick={() => {
+        setIsTimeSeries(!isTimeSeries);
+      }}
+    >
+      <div
+        className={
+          'bg-white md:w-6 md:h-6 h-5 w-5 rounded-full shadow-md transform' +
+          (isTimeSeries ? toggleClass : null)
+        }
+      ></div>
+    </div>
+  );
+
   return (
     <>
       <Head title="wozitto" description="About woitto" />
@@ -56,121 +78,249 @@ const AboutPage = () => {
 
             <Border />
 
-            <Heading level={2}>Works</Heading>
+            <div className="flex justify-between items-center">
+              <Heading level={2}>Works</Heading>
+              <TimeSeriesSwitch />
+            </div>
 
             <Timeline>
-              <Event>
-                <EventIconWrapper>
-                  <RiMoneyCnyCircleFill size={28} />
-                </EventIconWrapper>
-                <Month>2022/04</Month>
-                <EventTitle>Money Forward Inc.</EventTitle>
-                <ExternalLink href="https://corp.moneyforward.com/">
-                  https://corp.moneyforward.com/
-                </ExternalLink>
-                <EventDescription>
-                  マネーフォワードビジネスカンパニー クラウド経費本部 開発部
-                  フロントエンドグループ インターン
-                </EventDescription>
-                <EventDescription>フロントエンドエンジニア</EventDescription>
-                <EventNote>React, Redux, TypeScript, Rails</EventNote>
-              </Event>
+              {isTimeSeries ? (
+                <>
+                  <Event>
+                    <EventIconWrapper>
+                      <MdOutlineSportsSoccer size={26} />
+                    </EventIconWrapper>
+                    <Month>2019/04</Month>
+                    <EventTitle>東福岡高等学校 入学</EventTitle>
+                    <EventDescription>
+                      プロサッカー選手を目指し寮生活でサッカーに励んでいました。
+                    </EventDescription>
+                  </Event>
 
-              <Event>
-                <EventIconWrapper>
-                  <FaGraduationCap size={22} />
-                </EventIconWrapper>
-                <Month>2022/03</Month>
-                <EventTitle>N高等学校 卒業</EventTitle>
-              </Event>
+                  <Event>
+                    <EventIconWrapper>
+                      <FaSchool size={20} />
+                    </EventIconWrapper>
+                    <Month>2020/06</Month>
+                    <EventTitle>N高等学校 転入学</EventTitle>
+                    <EventDescription>
+                      サッカー選手からエンジニアに目標を変更し、学習時間の確保のためN高等学校に転入学
+                    </EventDescription>
+                  </Event>
 
-              <Event>
-                <EventIconWrapper>
-                  <BsTsunami size={20} />
-                </EventIconWrapper>
-                <EventTitle>Amazon商品管理システム</EventTitle>
-                <EventDescription>
-                  フロントエンド / バックエンド
-                </EventDescription>
-                <EventNote>
-                  Next.js, Recoil, Typescript, GraphQL, TailwindCSS,
-                  Rails-apimode
-                </EventNote>
-              </Event>
+                  <Event>
+                    <EventIconWrapper>
+                      <BsTsunami size={20} />
+                    </EventIconWrapper>
+                    <Month>2021/03</Month>
+                    <EventTitle>株式会社UKIYOcreate</EventTitle>
+                    <ExternalLink href="https://www.ukiyo-create.co.jp/">
+                      https://www.ukiyo-create.co.jp/
+                    </ExternalLink>
+                    <EventDescription>インターン</EventDescription>
+                    <EventDescription>フルスタックエンジニア</EventDescription>
+                    <EventNote>
+                      React, Redux, Redux-Toolkit, Recoil, TypeScript, GraphQL,
+                      Rails-Apimode
+                    </EventNote>
+                  </Event>
 
-              <Event>
-                <EventIconWrapper>
-                  <BsTsunami size={20} />
-                </EventIconWrapper>
-                <EventTitle>商標管理システム</EventTitle>
-                <EventDescription>
-                  フロントエンド / バックエンド
-                </EventDescription>
-                <EventNote>
-                  Next.js, Redux-Toolkit, Typescript, TailwindCSS, Rails-apimode
-                </EventNote>
-              </Event>
+                  <Event>
+                    <EventIconWrapper>
+                      <BsTsunami size={20} />
+                    </EventIconWrapper>
+                    <EventTitle>LP マークアップ</EventTitle>
+                    <EventNote>HTML, Sass, jQuery</EventNote>
+                  </Event>
 
-              <Event>
-                <EventIconWrapper>
-                  <BsTsunami size={20} />
-                </EventIconWrapper>
-                <EventTitle>離脱防止サービス Popee</EventTitle>
-                <ExternalLink href="https://popee.info/lp">
-                  https://popee.info/lp
-                </ExternalLink>
-                <EventDescription>フロントエンド</EventDescription>
-                <EventNote>
-                  React, Redux, Typescript, styled-components
-                </EventNote>
-              </Event>
+                  <Event>
+                    <EventIconWrapper>
+                      <BsTsunami size={20} />
+                    </EventIconWrapper>
+                    <EventTitle>離脱防止サービス Popee</EventTitle>
+                    <ExternalLink href="https://popee.info/lp">
+                      https://popee.info/lp
+                    </ExternalLink>
+                    <EventDescription>フロントエンド</EventDescription>
+                    <EventNote>
+                      React, Redux, Typescript, styled-components
+                    </EventNote>
+                  </Event>
 
-              <Event>
-                <EventIconWrapper>
-                  <BsTsunami size={20} />
-                </EventIconWrapper>
-                <EventTitle>LP マークアップ</EventTitle>
-                <EventNote>HTML, Sass, jQuery</EventNote>
-              </Event>
+                  <Event>
+                    <EventIconWrapper>
+                      <BsTsunami size={20} />
+                    </EventIconWrapper>
+                    <EventTitle>商標管理システム</EventTitle>
+                    <EventDescription>
+                      フロントエンド / バックエンド
+                    </EventDescription>
+                    <EventNote>
+                      Next.js, Redux-Toolkit, Typescript, TailwindCSS,
+                      Rails-apimode
+                    </EventNote>
+                  </Event>
 
-              <Event>
-                <EventIconWrapper>
-                  <BsTsunami size={20} />
-                </EventIconWrapper>
-                <Month>2021/03</Month>
-                <EventTitle>株式会社UKIYOcreate</EventTitle>
-                <ExternalLink href="https://www.ukiyo-create.co.jp/">
-                  https://www.ukiyo-create.co.jp/
-                </ExternalLink>
-                <EventDescription>インターン</EventDescription>
-                <EventDescription>フルスタックエンジニア</EventDescription>
-                <EventNote>
-                  React, Redux, Redux-Toolkit, Recoil, TypeScript, GraphQL,
-                  Rails-Apimode
-                </EventNote>
-              </Event>
+                  <Event>
+                    <EventIconWrapper>
+                      <BsTsunami size={20} />
+                    </EventIconWrapper>
+                    <EventTitle>Amazon商品管理システム</EventTitle>
+                    <EventDescription>
+                      フロントエンド / バックエンド
+                    </EventDescription>
+                    <EventNote>
+                      Next.js, Recoil, Typescript, GraphQL, TailwindCSS,
+                      Rails-apimode
+                    </EventNote>
+                  </Event>
 
-              <Event>
-                <EventIconWrapper>
-                  <FaSchool size={20} />
-                </EventIconWrapper>
-                <Month>2020/06</Month>
-                <EventTitle>N高等学校 転入学</EventTitle>
-                <EventDescription>
-                  サッカー選手からエンジニアに目標を変更し、学習時間の確保のためN高等学校に転入学
-                </EventDescription>
-              </Event>
+                  <Event>
+                    <EventIconWrapper>
+                      <FaGraduationCap size={22} />
+                    </EventIconWrapper>
+                    <Month>2022/03</Month>
+                    <EventTitle>N高等学校 卒業</EventTitle>
+                  </Event>
 
-              <Event>
-                <EventIconWrapper>
-                  <MdOutlineSportsSoccer size={26} />
-                </EventIconWrapper>
-                <Month>2019/04</Month>
-                <EventTitle>東福岡高等学校 入学</EventTitle>
-                <EventDescription>
-                  プロサッカー選手を目指し寮生活でサッカーに励んでいました。
-                </EventDescription>
-              </Event>
+                  <Event>
+                    <EventIconWrapper>
+                      <RiMoneyCnyCircleFill size={28} />
+                    </EventIconWrapper>
+                    <Month>2022/04</Month>
+                    <EventTitle>Money Forward Inc.</EventTitle>
+                    <ExternalLink href="https://corp.moneyforward.com/">
+                      https://corp.moneyforward.com/
+                    </ExternalLink>
+                    <EventDescription>
+                      マネーフォワードビジネスカンパニー クラウド経費本部 開発部
+                      フロントエンドグループ インターン
+                    </EventDescription>
+                    <EventDescription>
+                      フロントエンドエンジニア
+                    </EventDescription>
+                    <EventNote>React, Redux, TypeScript, Rails</EventNote>
+                  </Event>
+                </>
+              ) : (
+                <>
+                  <Event>
+                    <EventIconWrapper>
+                      <RiMoneyCnyCircleFill size={28} />
+                    </EventIconWrapper>
+                    <Month>2022/04</Month>
+                    <EventTitle>Money Forward Inc.</EventTitle>
+                    <ExternalLink href="https://corp.moneyforward.com/">
+                      https://corp.moneyforward.com/
+                    </ExternalLink>
+                    <EventDescription>
+                      マネーフォワードビジネスカンパニー クラウド経費本部 開発部
+                      フロントエンドグループ インターン
+                    </EventDescription>
+                    <EventDescription>
+                      フロントエンドエンジニア
+                    </EventDescription>
+                    <EventNote>React, Redux, TypeScript, Rails</EventNote>
+                  </Event>
+
+                  <Event>
+                    <EventIconWrapper>
+                      <FaGraduationCap size={22} />
+                    </EventIconWrapper>
+                    <Month>2022/03</Month>
+                    <EventTitle>N高等学校 卒業</EventTitle>
+                  </Event>
+
+                  <Event>
+                    <EventIconWrapper>
+                      <BsTsunami size={20} />
+                    </EventIconWrapper>
+                    <EventTitle>Amazon商品管理システム</EventTitle>
+                    <EventDescription>
+                      フロントエンド / バックエンド
+                    </EventDescription>
+                    <EventNote>
+                      Next.js, Recoil, Typescript, GraphQL, TailwindCSS,
+                      Rails-apimode
+                    </EventNote>
+                  </Event>
+
+                  <Event>
+                    <EventIconWrapper>
+                      <BsTsunami size={20} />
+                    </EventIconWrapper>
+                    <EventTitle>商標管理システム</EventTitle>
+                    <EventDescription>
+                      フロントエンド / バックエンド
+                    </EventDescription>
+                    <EventNote>
+                      Next.js, Redux-Toolkit, Typescript, TailwindCSS,
+                      Rails-apimode
+                    </EventNote>
+                  </Event>
+
+                  <Event>
+                    <EventIconWrapper>
+                      <BsTsunami size={20} />
+                    </EventIconWrapper>
+                    <EventTitle>離脱防止サービス Popee</EventTitle>
+                    <ExternalLink href="https://popee.info/lp">
+                      https://popee.info/lp
+                    </ExternalLink>
+                    <EventDescription>フロントエンド</EventDescription>
+                    <EventNote>
+                      React, Redux, Typescript, styled-components
+                    </EventNote>
+                  </Event>
+
+                  <Event>
+                    <EventIconWrapper>
+                      <BsTsunami size={20} />
+                    </EventIconWrapper>
+                    <EventTitle>LP マークアップ</EventTitle>
+                    <EventNote>HTML, Sass, jQuery</EventNote>
+                  </Event>
+
+                  <Event>
+                    <EventIconWrapper>
+                      <BsTsunami size={20} />
+                    </EventIconWrapper>
+                    <Month>2021/03</Month>
+                    <EventTitle>株式会社UKIYOcreate</EventTitle>
+                    <ExternalLink href="https://www.ukiyo-create.co.jp/">
+                      https://www.ukiyo-create.co.jp/
+                    </ExternalLink>
+                    <EventDescription>インターン</EventDescription>
+                    <EventDescription>フルスタックエンジニア</EventDescription>
+                    <EventNote>
+                      React, Redux, Redux-Toolkit, Recoil, TypeScript, GraphQL,
+                      Rails-Apimode
+                    </EventNote>
+                  </Event>
+
+                  <Event>
+                    <EventIconWrapper>
+                      <FaSchool size={20} />
+                    </EventIconWrapper>
+                    <Month>2020/06</Month>
+                    <EventTitle>N高等学校 転入学</EventTitle>
+                    <EventDescription>
+                      サッカー選手からエンジニアに目標を変更し、学習時間の確保のためN高等学校に転入学
+                    </EventDescription>
+                  </Event>
+
+                  <Event>
+                    <EventIconWrapper>
+                      <MdOutlineSportsSoccer size={26} />
+                    </EventIconWrapper>
+                    <Month>2019/04</Month>
+                    <EventTitle>東福岡高等学校 入学</EventTitle>
+                    <EventDescription>
+                      プロサッカー選手を目指し寮生活でサッカーに励んでいました。
+                    </EventDescription>
+                  </Event>
+                </>
+              )}
             </Timeline>
           </main>
         </PageTransition>
